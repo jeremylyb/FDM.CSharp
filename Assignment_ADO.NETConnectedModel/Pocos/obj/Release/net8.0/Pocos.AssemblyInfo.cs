a32ddcf86e120947a2fb8dd31f5f4ed85afc99fa
jeremylyb <96461868+jeremylyb@users.noreply.github.com>
2024-03-15 17:26:25 +0800

    Testing upload new test folder

--- Assignment_ADO.NETConnectedModel/Pocos/obj/Release/net8.0/Pocos.AssemblyInfo.cs
+++ Assignment_ADO.NETConnectedModel/Pocos/obj/Release/net8.0/Pocos.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pocos")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1148e72c7a735048e64dc75e609c1660baf04dd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pocos")]
